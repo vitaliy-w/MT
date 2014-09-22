fa73d5f9029c428bce59e19ddc06bdcc5bb0507c
Drag13 <qwark@list.ru>
2014-09-22 20:05:01 +0300

    Update Localisations.cs

--- MT.Web/MT.ModelEntities/Localisations.cs
+++ MT.Web/MT.ModelEntities/Localisations.cs
@@ -3,21 +3,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MT.ModelEntities.Entities
 {
-   public class LocalisationTable
+   public class LocalisationResource
     {
      
-       [Key, Column(Order = 0)]
+       [Key, Column(Order = 0)] // Первая часть составного ключа
        [Required]
-       [StringLength(100, ErrorMessage = "More than 100 in ResourceKey symbols are not allowed")]
+       [StringLength(100, ErrorMessage = "More than 100 in ResourceKey symbols are not allowed")] // Максимальная длинна 100 символов
        public string ResourceKey { get; set; }
 
-       [Key, Column(Order = 1)]
+       [Key, Column(Order = 1)] //вторая часть составного ключа
        [Required]
-       [StringLength(5, ErrorMessage = "More than 5 symbols in ResourceCulture Code are not allowed")]
+       [StringLength(5, ErrorMessage = "More than 5 symbols in ResourceCulture Code are not allowed")] // Максимальная длинна 5 символов
        public string ResourceCultureCode { get; set; }
 
        [Required]
-       public string LocalizedResource { get; set; }
+       public string LocalizedResource { get; set; } // собственно сам ресурс для локализации
 
        
     }
