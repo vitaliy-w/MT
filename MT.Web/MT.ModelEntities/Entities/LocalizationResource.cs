e3c2e3f9580111c155c201e893e2e266991d1977
Drag13 <qwark@list.ru>
2014-09-23 10:50:49 +0300

    LocalisationResource

---
+++ MT.Web/MT.ModelEntities/Entities/LocalizationResource.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace MT.ModelEntities.Entities
+{
+    public class LocalisationResource
+    {
+        /// <remarks>
+        ///  Первая часть составного ключа
+        /// Максимальная длинна ключа 100 символов
+        /// </remarks>>
+        [Key, Column(Order = 0)] 
+        [Required]
+        [StringLength(100, ErrorMessage = "More than 100 in ResourceKey symbols are not allowed" )] 
+        public string ResourceKey { get; set; }
+
+        /// <remarks>
+        /// Вторая часть составного ключа. Ключ культуры языка в формате en-US, ru-RU, en-GB
+        /// Максимальная длинна 5 символов
+        /// </remarks>
+        [Key, Column(Order = 1)] 
+        [Required]
+        [StringLength(5, ErrorMessage = "More than 5 symbols in ResourceCulture Code are not allowed")] 
+        public string ResourceCultureCode { get; set; }
+
+
+        /// <remarks>
+        /// Cам ресурс для локализации
+        /// </remarks>
+        [Required]
+        public string LocalizedResource { get; set; } 
+        
+    }
+
+
+
+
+
+
+}
