9c2cc227dc7611c0b804881f10284168c280cbc0
Vitaliy Dyachenko <d9chenkovitaliy@gmail.com>
2014-09-23 12:00:10 +0300

    MT-6: refactoring

--- MT.Web/MT.ModelEntities/Entities/LocalizationResource.cs
+++ MT.Web/MT.ModelEntities/Entities/LocalizationResource.cs
@@ -2,33 +2,36 @@
 using System.ComponentModel.DataAnnotations;
 namespace MT.ModelEntities.Entities
 {
+    /// <summary>
+    /// Describes the text of different languages and cultures.
+    /// </summary>
     public class LocalisationResource
     {
-        /// <remarks>
-        ///  Первая часть составного ключа
-        /// Максимальная длинна ключа 100 символов
-        /// </remarks>>
-        [Key, Column(Order = 0)] 
+        /// <summary>
+        ///  Строка, которая являеться уникальным индефикатором для конкретного текста.
+        /// Максимальная длинна 100 символов.
+        /// </summary>>
+        [Key, Column(Order = 0)]
         [Required]
-        [StringLength(100, ErrorMessage = "More than 100 in ResourceKey symbols are not allowed" )] 
+        [StringLength(100, ErrorMessage = "More than 100 in ResourceKey symbols are not allowed")]
         public string ResourceKey { get; set; }
 
-        /// <remarks>
-        /// Вторая часть составного ключа. Ключ культуры языка в формате en-US, ru-RU, en-GB
-        /// Максимальная длинна 5 символов
-        /// </remarks>
-        [Key, Column(Order = 1)] 
+        /// <summary>
+        /// Культура языка в формате en-US, ru-RU, en-GB.
+        /// Максимальная длинна 5 символов.
+        /// </summary>
+        [Key, Column(Order = 1)]
         [Required]
-        [StringLength(5, ErrorMessage = "More than 5 symbols in ResourceCulture Code are not allowed")] 
+        [StringLength(5, ErrorMessage = "More than 5 symbols in ResourceCulture Code are not allowed")]
         public string ResourceCultureCode { get; set; }
 
 
-        /// <remarks>
-        /// Cам ресурс для локализации
-        /// </remarks>
+        /// <summary>
+        /// Переведенный текст на соответствующий язык.
+        /// </summary>
         [Required]
-        public string LocalizedResource { get; set; } 
-        
+        public string LocalizedResource { get; set; }
+
     }
 
 
