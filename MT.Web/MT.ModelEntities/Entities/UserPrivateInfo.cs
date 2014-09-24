b84ed839154a5caa0b373f2f0f32fd4e6404be9f
JoSaint <Andrey.Zherditskiy@outlook.com>
2014-09-24 23:06:31 +0300

    MT-7 Domain Class

---
+++ MT.Web/MT.ModelEntities/Entities/UserPrivateInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.ModelEntities.Entities
+{
+    public class UserPrivateInfo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "You have to enter your Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "You have to enter your LastName")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "You have to enter your Sex")]
+        public string Sex { get; set; }
+
+        [Required]
+        public DateTimeOffset TimeZone { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "You have to enter where are you from, yuor Country")]
+        public string Country { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "You have to enter where are you from, your City")]
+        public string City { get; set; }
+    }
+}
