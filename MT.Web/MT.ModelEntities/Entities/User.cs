d6e3daa963f6590b7da73d230adc22cd5c603cc9
Zhurs11 <soulfly.jah@gmail.com>
2014-09-29 23:21:14 +0300

    расширил сущность User

--- MT.Web/MT.ModelEntities/Entities/User.cs
+++ MT.Web/MT.ModelEntities/Entities/User.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MT.ModelEntities.Entities
 {
@@ -10,5 +12,27 @@ namespace MT.ModelEntities.Entities
         [MaxLength(50)]
         [Required]
         public string UserName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный адрес")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Пароль должен быть минимум 6 символов")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Подтверждение пароля (поле не сохраняется в БД)
+        /// </summary>
+        [NotMapped]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+
+        /// <summary>
+        /// Время создания пользователя
+        /// </summary>
+        public DateTime? Created { get; set; }
+
     }
 }
