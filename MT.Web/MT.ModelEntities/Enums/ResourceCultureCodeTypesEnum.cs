1bae8c192ed46d356beb531f653529b247453f64
Drag13 <qwark@list.ru>
2014-10-13 17:59:50 +0300

    Добавляет:
    1. Индекс страницу со списком всех ресурсов локализации. Вынести джс код в отдельный файл не смог, т.к. тогда не работает var obj = '@Html.ToJson(Model)';
    2. AddLocalizationResource.cshtml которая позволяет добавлять новый ресурс. Результат выводиться на контрол алерт, джс код вынесен в отдельный файл.
    3. ActionLinkDirective обертка для  ActionLinkDirective
    4. DropDownListDirective - перегруженный. Но нужно будет согласовать оба метода.

---
+++ MT.Web/MT.ModelEntities/Enums/ResourceCultureCodeTypesEnum.cs
@@ -0,0 +1,16 @@
+﻿using MT.Utility.Localization.Attributes;
+
+namespace MT.ModelEntities.Enums
+{
+    /// <summary>
+    /// Represents culture codes.
+    /// </summary>
+    public enum ResourceCultureCodeTypesEnum
+    {
+        [EnumDisplayNameAttribute("ru-RU")]
+        RuRu = 0,
+        [EnumDisplayNameAttribute("en-US")]
+        EnUs = 1
+
+    };
+}
---
+++ MT.Web/MT.Web/Areas/Management/Views/Localization/AddLocalizationResource.js
@@ -0,0 +1,29 @@
+﻿angular.module('mtApp').controller('createLocalizationResource',
+function ($scope, validationService, createLocalizationService) {
+
+    $scope.resource = {};
+    $scope.isSectionVisible = [false];
+    $scope.resource.LocalizedResources = [];  //Немного уличной магии. Если не создавать массив явно, не сработает dataBinding в контроллере.
+    $scope.resource.ResourceCultureCodes = [];
+
+    // formName - form name for validation
+    $scope.create = function (formName) {
+        if (!validationService.isValid(formName))
+            return;
+        createLocalizationService.saveResource($scope.resource);
+
+    };
+});
+
+// Fabric method for saving LibraryResource instance using httpService
+angular.module('mtApp').factory('createLocalizationService', function (httpService) {
+    return {
+        saveResource: function (resource) {
+            return httpService.post('Management/Localization/Create', resource).then(function (responce) {
+                var x = document.getElementById("msg_from_server");
+                x.innerHTML = responce;
+            });
+        }
+    };
+});
+
