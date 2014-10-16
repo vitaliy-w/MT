859a513e6aac18cce38b7fb81f286ebbb8f959ba
Ruslan Levkov <agoge@outlook.com>
2014-10-16 14:10:09 +0300

    MT-13: Add files interface IUserLoginService and his implementation UserLoginService

---
+++ MT.Web/MT.DomainLogic/IUserLoginService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace MT.DomainLogic
+{
+    /// <summary>
+    /// Represents the functionality for work with user authorization
+    /// </summary>
+    public interface IUserLoginService
+    {
+        /// <summary>
+        /// Check if is registered user with set email and password
+        /// </summary>
+        /// <returns>Returns True if user with set parameters exists, otherwise returns False</returns>
+        bool ValidateUser(string email, string password);
+
+        /// <summary>
+        /// Records user's login attempt
+        /// </summary>
+        /// <param name="userId">User's ID</param>
+        /// <param name="date">Date when occur login attempt</param>
+        /// <param name="result">The Success or the Failure of login attempt</param>
+        void SetUserLoginHistory(int userId, DateTime date, bool result);
+
+        /// <summary>
+        /// Retrieves the user ID from given email
+        /// </summary>
+        /// <param name="email">User's email</param>
+        /// <returns>Returns the user's ID if one exist, otherwise "-1"</returns>
+        int GetUserIdFromEmail(string email);
+
+        /// <summary>
+        /// Check if user with given ID is banned
+        /// </summary>
+        /// <param name="userId">User's ID</param>
+        /// <returns>Returns True if current user is banned, otherwise - False</returns>
+        bool UserIsBan(int userId);
+
+        /// <summary>
+        /// Set the user's ban status to True or False
+        /// </summary>
+        /// <param name="userId">User's ID</param>
+        /// <param name="ban">Sets True if need to ban current user, otherwise - False</param>
+        void SetUserBan(int userId, bool ban);
+
+        /// <summary>
+        /// Retrives time when user was banned
+        /// </summary>
+        /// <param name="userId">User's ID</param>
+        TimeSpan GetBanTime(int userId);
+
+        /// <summary>
+        /// Sets time when user was banned
+        /// </summary>
+        /// <param name="userId">User's ID</param>
+        void SetStartBanTime(int userId);
+
+        /// <summary>
+        /// Retrieves the value user login attempt count
+        /// </summary>
+        /// <param name="userId">User's ID</param>
+        int GetCountAttempt(int userId);
+
+        /// <summary>
+        /// Increase the attempt count value on one
+        /// </summary>
+        /// <param name="userId">User's ID</param>
+        void SetCountAttemptToPlusOne(int userId);
+
+        /// <summary>
+        /// Set the attempt count value to zero
+        /// </summary>
+        /// <param name="userId">User's ID</param>
+        void SetCountAttemptToZero(int userId);
+    }
+}
---
+++ MT.Web/MT.DomainLogic/UserLoginService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using MT.DataAccess.EntityFramework;
+using MT.ModelEntities.Entities;
+
+namespace MT.DomainLogic
+{
+    public class UserLoginService : IUserLoginService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserLoginService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public bool ValidateUser(string email, string password)
+        {
+            return _unitOfWork.Get<User>().Any(u => u.Email == email && u.Password == password);
+        }
+
+        public void SetUserLoginHistory(int userId, DateTime date, bool result)
+        {
+            _unitOfWork.Add(new UserLoginHistory { UserId = userId, LoginDate = date, LoginResult = result });
+        }
+
+        public int GetUserIdFromEmail(string email)
+        {
+            try
+            {
+                return _unitOfWork.Get<User>().Where(u => u.Email == email).Select(u => u.Id).First();
+            }
+            catch (Exception)
+            {
+
+                return -1;
+            }
+        }
+
+        public bool UserIsBan(int userId)
+        {
+            return _unitOfWork.Get<UserBan>().Where(u => u.UserId == userId).Select(u => u.UserIsBan).First();
+        }
+
+        public void SetUserBan(int userId, bool ban)
+        {
+            var userBan = _unitOfWork.Get<UserBan>().First(u => u.UserId == userId);
+            userBan.UserIsBan = ban;
+        }
+
+        public TimeSpan GetBanTime(int userId)
+        {
+            var startBanTime = _unitOfWork.Get<UserBan>().Where(u => u.UserId == userId).Select(u => u.StartBanTime).First();
+            var timeNow = DateTime.Now;
+            var banTime = timeNow - startBanTime;
+            return banTime;
+        }
+
+        public void SetStartBanTime(int userId)
+        {
+            var userBan = _unitOfWork.Get<UserBan>().First(u => u.UserId == userId);
+            userBan.StartBanTime = DateTime.Now;
+        }
+
+        public int GetCountAttempt(int userId)
+        {
+            return _unitOfWork.Get<UserBan>().Where(u => u.UserId == userId).Select(u => u.AttemptCount).First();
+        }
+
+        public void SetCountAttemptToPlusOne(int userId)
+        {
+            var userBan = _unitOfWork.Get<UserBan>().First(u => u.UserId == userId);
+            userBan.AttemptCount++;
+        }
+
+        public void SetCountAttemptToZero(int userId)
+        {
+            var userBan = _unitOfWork.Get<UserBan>().First(u => u.UserId == userId);
+            userBan.AttemptCount = 0;
+        }
+    }
+}
